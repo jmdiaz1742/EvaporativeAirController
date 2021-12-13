5143bb0afbdb37210252a0e84039633ed9499bb7
Miguel Diaz <jose.diaz@resideo.com>
2021-12-13 16:42:42 -0700

    Formatting file

--- guiTest.py
+++ guiTest.py
@@ -10,6 +10,7 @@ time_hold = False
 hold_until_time = datetime.now()
 time_holding_step = 15
 
+
 def motor_button_clicked():
     global motor_speed
     global motor_speed_text
@@ -24,6 +25,7 @@ def motor_button_clicked():
         motor_text.text_color = 'black'
     motor_text.value = motor_speed_text[motor_speed]
     print("speed: " + motor_speed_text[motor_speed])
+
 
 def pump_button_clicked():
     global pump_state
@@ -40,6 +42,7 @@ def pump_button_clicked():
     pump_text.value = pump_state_text[pump_state]
     print("Pump: " + pump_state_text[pump_state])
 
+
 def time_subs_button_clicked():
     global time_hold
     global cancel_hold_button
@@ -48,6 +51,7 @@ def time_subs_button_clicked():
     adjust_hold_time(-time_holding_step)
     if (time_hold):
         cancel_hold_button.enable()
+
 
 def time_add_button_clicked():
     global time_hold
@@ -58,15 +62,17 @@ def time_add_button_clicked():
     if (time_hold):
         cancel_hold_button.enable()
 
+
 def cancel_hold_button_clicked():
     global cancel_hold_button
     global time_subs_button
-    
+
     adjust_hold_time(0)
     cancel_hold_button.disable()
     time_subs_button.disable()
 
-def adjust_hold_time(delta_time = 0):
+
+def adjust_hold_time(delta_time=0):
     global time_hold
     global hold_until_time
     global time_subs_button
@@ -79,7 +85,7 @@ def adjust_hold_time(delta_time = 0):
             # If we're starting the hold, use current time
             hold_until_time = datetime.now()
 
-        hold_until_time += timedelta(minutes = delta_time)
+        hold_until_time += timedelta(minutes=delta_time)
         print("New hold until time: " + str(hold_until_time.second))
         time_hold = True
 
@@ -87,6 +93,7 @@ def adjust_hold_time(delta_time = 0):
     if time_hold:
         check_remaining_time()
         print("Hold until: " + hold_until_time.strftime("%H:%M"))
+
 
 def refresh_time():
     global time_hold
@@ -105,6 +112,8 @@ def refresh_time():
         time_subs_button.disable()
 
 # Run every second to check timer and holds
+
+
 def check_remaining_time():
     global time_hold
     global hold_until_time
@@ -113,7 +122,7 @@ def check_remaining_time():
 
     # No need to check anything if we don't have a hold
     if (time_hold):
-        if (hold_until_time >= (datetime.now() + timedelta(minutes = time_holding_step))):
+        if (hold_until_time >= (datetime.now() + timedelta(minutes=time_holding_step))):
             time_subs_button.enable()
         else:
             time_subs_button.disable()
@@ -124,6 +133,7 @@ def check_remaining_time():
             adjust_hold_time(0)
         print("Remaining time: " + str(remaining_time.total_seconds()))
 
+
 # UI elements
 element_height = 3
 button_width = 10
@@ -132,16 +142,62 @@ text_width = 12
 font_size = 14
 picture_size = 100
 
-app = App(title="Cool Air Controller", width=480, height=320, layout="grid")
-frost_picture = Picture(app, image="frost.png", grid=[3,0], height=picture_size, width=picture_size)
-motor_button = PushButton(app, command=motor_button_clicked, text="Motor speed", grid=[0, 0], width=button_width, height=element_height)
-motor_text = Text(app, text=motor_speed_text[motor_speed], grid=[1, 0], size=font_size)
-pump_button = PushButton(app, command=pump_button_clicked, text="Pump", grid=[0, 1], width=button_width, height=element_height)
-pump_text = Text(app, text=pump_state_text[pump_state], grid=[1, 1], size=font_size)
-cancel_hold_button = PushButton(app, command=cancel_hold_button_clicked, text="cancel", enabled=False, grid=[0, 2], width=button_width, height=element_height)
-time_text = Text(app, text=str("Current time:\n" + datetime.now().strftime("%H:%M")), grid=[1, 2], width=text_width, height=element_height, size=font_size)
-time_subs_button = PushButton(app, command=time_subs_button_clicked, text="-", enabled=False, grid=[2, 2], width=time_button_width, height=element_height)
-time_add_button = PushButton(app, command=time_add_button_clicked, text="+", grid=[3, 2], width=time_button_width, height=element_height)
+app = App(title="Cool Air Controller",
+          width=480,
+          height=320,
+          layout="grid")
+frost_picture = Picture(app,
+                        image="frost.png",
+                        grid=[3, 0],
+                        height=picture_size,
+                        width=picture_size)
+motor_button = PushButton(app,
+                          command=motor_button_clicked,
+                          text="Motor speed",
+                          grid=[0, 0],
+                          width=button_width,
+                          height=element_height)
+motor_text = Text(app,
+                  text=motor_speed_text[motor_speed],
+                  grid=[1, 0],
+                  size=font_size)
+pump_button = PushButton(app,
+                         command=pump_button_clicked,
+                         text="Pump",
+                         grid=[0, 1],
+                         width=button_width,
+                         height=element_height)
+pump_text = Text(app,
+                 text=pump_state_text[pump_state],
+                 grid=[1, 1],
+                 size=font_size)
+cancel_hold_button = PushButton(app,
+                                command=cancel_hold_button_clicked,
+                                text="cancel",
+                                enabled=False,
+                                grid=[0, 2],
+                                width=button_width,
+                                height=element_height)
+time_text = Text(app,
+                 text=str("Current time:\n" +
+                          datetime.now().strftime("%H:%M")),
+                 grid=[1, 2],
+                 width=text_width,
+                 height=element_height,
+                 size=font_size)
+time_subs_button = PushButton(app,
+                              command=time_subs_button_clicked,
+                              text="-",
+                              enabled=False,
+                              grid=[2, 2],
+                              width=time_button_width,
+                              height=element_height)
+time_add_button = PushButton(app,
+                             command=time_add_button_clicked,
+                             text="+",
+                             grid=[3, 2],
+                             width=time_button_width,
+                             height=element_height)
 
 # Set buttons text size
 motor_button.text_size = font_size
