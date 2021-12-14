20d7824e66acc3e847845b73bee4798c9064bd36
Miguel <jmdiaz1742@gmail.com>
2021-12-14 13:43:55 -0700

    Fixing Cancel behavior
    Adding colors, might not be final

--- guiTest.py
+++ guiTest.py
@@ -1,46 +1,31 @@
 from guizero import App, Text, PushButton, Picture
-from datetime import time, datetime, timedelta
+from datetime import datetime, timedelta
+import time
 import argparse
 try:
     import RPi.GPIO as GPIO
     relays_enabled = True
 except ImportError:
-    print("Running in desktop probably")
+    print("Probably Running in desktop")
     relays_enabled = False
 
 
 def motor_button_clicked():
     global motor_speed
-    global motor_speed_text
-    global motor_text
 
     motor_speed += 1
     if (motor_speed >= len(motor_speed_text)):
         motor_speed = 0
-    if motor_speed > 0:
-        motor_text.text_color = 'green'
-    else:
-        motor_text.text_color = 'black'
-    motor_text.value = motor_speed_text[motor_speed]
     set_motor_speed(motor_speed)
-    print("speed: " + motor_speed_text[motor_speed])
 
 
 def pump_button_clicked():
     global pump_state
-    global pump_state_text
-    global pump_text
 
     pump_state += 1
     if (pump_state >= len(pump_state_text)):
         pump_state = 0
-    if pump_state > 0:
-        pump_text.text_color = 'green'
-    else:
-        pump_text.text_color = 'black'
-    pump_text.value = pump_state_text[pump_state]
     set_pump(pump_state)
-    print("Pump: " + pump_state_text[pump_state])
 
 
 def time_subs_button_clicked():
@@ -72,6 +57,14 @@ def cancel_hold_button_clicked():
     time_subs_button.disable()
 
 
+def exit_button_clicked():
+    global app
+
+    turn_off_all()
+    app.destroy()
+    print("Goodbye!")
+
+
 def adjust_hold_time(delta_time=0):
     global time_hold
     global hold_until_time
@@ -91,7 +84,6 @@ def adjust_hold_time(delta_time=0):
 
     refresh_time()
     if time_hold:
-        check_remaining_time()
         print("Hold until: " + hold_until_time.strftime("%H:%M"))
 
 
@@ -101,13 +93,15 @@ def refresh_time():
     global hold_until_time
     global cancel_hold_button
     global time_subs_button
+    global text_color_active
+    global text_color_inactive
 
     if (time_hold):
         time_text.value = "Hold until:\n" + hold_until_time.strftime("%H:%M")
-        time_text.text_color = 'green'
+        time_text.text_color = text_color_active
     else:
         time_text.value = "Current time:\n" + datetime.now().strftime("%H:%M")
-        time_text.text_color = 'black'
+        time_text.text_color = text_color_inactive
         cancel_hold_button.disable()
         time_subs_button.disable()
         turn_off_all()
@@ -117,7 +111,6 @@ def check_remaining_time():
     # Run every second to check timer and holds
     global time_hold
     global hold_until_time
-    global time_text
     global time_subs_button
     global time_holding_step
 
@@ -129,10 +122,13 @@ def check_remaining_time():
             time_subs_button.disable()
 
         remaining_time = hold_until_time - datetime.now()
-        if (remaining_time.total_seconds() < 1):
+        if (remaining_time.total_seconds() < 0):
+            # Time's up, we'll cancel the hold, and turn off everything
+            print("Time's up")
             time_hold = False
             adjust_hold_time(0)
-        print("Remaining time: " + str(remaining_time.total_seconds()))
+        else:
+            print("Remaining time: " + str(remaining_time.total_seconds()))
 
 
 def init_pins():
@@ -142,7 +138,7 @@ def init_pins():
     global pin_pump
 
     if (relays_enabled):
-        GPIO.setmode(GPIO.BOARD)
+        GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin_motor_low, GPIO.OUT)
         GPIO.setup(pin_motor_high, GPIO.OUT)
         GPIO.setup(pin_pump, GPIO.OUT)
@@ -152,33 +148,60 @@ def set_motor_speed(speed):
     global relays_enabled
     global pin_motor_low
     global pin_motor_high
+    global motor_speed_text
+    global motor_text
+    global text_color_active
+    global text_color_inactive
 
     if (relays_enabled):
         # First shut down all motor relays, and wait a bit
         GPIO.output(pin_motor_low, GPIO.LOW)
         GPIO.output(pin_motor_low, GPIO.LOW)
-        time.sleep(250)
+        time.sleep(0.25)
 
         if (speed == 1):
             GPIO.output(pin_motor_low, GPIO.HIGH)
         elif (speed == 2):
             GPIO.output(pin_motor_high, GPIO.HIGH)
 
+    if speed > 0:
+        motor_text.text_color = text_color_active
+    else:
+        motor_text.text_color = text_color_inactive
+    motor_text.value = motor_speed_text[speed]
+    print("Motor: " + motor_speed_text[speed])
 
-def set_pump(pump):
+
+def set_pump(state):
     global relays_enabled
     global pin_pump
+    global pump_state_text
+    global pump_text
+    global text_color_active
+    global text_color_inactive
 
     if (relays_enabled):
-        if (pin_pump == 0):
+        if (state == 0):
             GPIO.output(pin_pump, GPIO.LOW)
-        elif (pin_pump == 1):
+        elif (state == 1):
             GPIO.output(pin_pump, GPIO.HIGH)
+
+    if state > 0:
+        pump_text.text_color = text_color_active
+    else:
+        pump_text.text_color = text_color_inactive
+    pump_text.value = pump_state_text[state]
+    print("Pump: " + pump_state_text[state])
 
 
 def turn_off_all():
-    set_motor_speed(0)
-    set_pump(0)
+    global motor_speed
+    global pump_state
+
+    motor_speed = 0
+    pump_state = 0
+    set_motor_speed(motor_speed)
+    set_pump(pump_state)
     print("System Off")
 
 
@@ -189,28 +212,29 @@ pump_state = 0
 pump_state_text = ["Off", "On"]
 time_hold = False
 hold_until_time = datetime.now()
-time_holding_step = 15
+time_holding_step = 30
 pin_motor_low = 5
-pin_motor_high = 5
-pin_pump = 13
+pin_motor_high = 6
+pin_pump = 26
 
 # UI elements
 element_height = 3
 button_width = 10
-time_button_width = 5
+time_button_width = 4
 text_width = 12
-font_size = 14
+exit_button_size = 1
+font_size = 16
 picture_size = 100
+bg_color = "#393F3F"
+text_color_default = "white"
+text_color_active = "#28C7CF"
+text_color_inactive = "light gray"
 
 app = App(title="Cool Air Controller",
-          width=480,
-          height=320,
-          layout="grid")
-frost_picture = Picture(app,
-                        image="frost.png",
-                        grid=[3, 0],
-                        height=picture_size,
-                        width=picture_size)
+          #   width=480,
+          #   height=320,
+          layout="grid",
+          bg=bg_color)
 motor_button = PushButton(app,
                           command=motor_button_clicked,
                           text="Motor speed",
@@ -258,13 +282,29 @@ time_add_button = PushButton(app,
                              grid=[3, 2],
                              width=time_button_width,
                              height=element_height)
+exit_button = PushButton(app,
+                         command=exit_button_clicked,
+                         text="X",
+                         grid=[3, 0],
+                         width=exit_button_size,
+                         height=exit_button_size,
+                         align="top")
 
 # Set buttons text size
 motor_button.text_size = font_size
+motor_button.text_color = text_color_default
 pump_button.text_size = font_size
+pump_button.text_color = text_color_default
 cancel_hold_button.text_size = font_size
+cancel_hold_button.text_color = text_color_default
 time_subs_button.text_size = font_size
+time_subs_button.text_color = text_color_default
 time_add_button.text_size = font_size
+time_add_button.text_color = text_color_default
+
+motor_text.text_color = text_color_inactive
+pump_text.text_color = text_color_inactive
+time_text.text_color = text_color_inactive
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dry", action="store_true",
@@ -278,5 +318,6 @@ if (args.dry):
 init_pins()
 turn_off_all()
 time_text.repeat(1000, check_remaining_time)
-# app.set_full_screen()
+if (relays_enabled):
+    app.set_full_screen()
 app.display()
